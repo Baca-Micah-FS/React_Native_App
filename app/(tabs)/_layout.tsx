755251e374c38605397f7d97f6f1fbd48da5e7aa
Micah <MJBaca@student.fullsail.edu>
2025-10-17 20:49:29 -0500

    Empty commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -23,7 +23,7 @@ export default function TabLayout() {
         options={{
           title: "Home",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="heart.fill" color={color} />
+            <IconSymbol size={28} name="house.fill" color={color} />
           ),
         }}
       />
